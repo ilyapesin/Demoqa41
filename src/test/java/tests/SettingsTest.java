2e5aeda209f320edb42d296fade38a08299be122
ilyapesin <pesinilya@gmail.com>
2024-02-29 22:00:53 +0200

    Init

---
+++ src/test/java/tests/SettingsTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class SettingsTest extends BaseTest{
+
+    @Test
+
+    public void test() throws InterruptedException {
+        Thread.sleep(3000);
+
+    }
+}
